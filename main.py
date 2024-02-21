bc83e53dfc803fb74f19647a77573a7a6b7ee805
jorge-ovalle <jorge.ovalle@uc.cl>
2024-02-21 18:16:35 -0300

    avances mas

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from simulador import AnugaSW
 import pandas as pd
 import parameters as p
 import pickle
+from procesamiento_archivos import df_to_asc
+from copy import deepcopy
+import os
 
 ''' PARÁMETROS '''
 
@@ -18,14 +21,15 @@ RUTA_EXTENSION_SECTOR_5 = f'{RUTA_FOLDER_DATA}extension_sector_5.csv'
 RUTA_EXTENSION_SECTOR_6 = f'{RUTA_FOLDER_DATA}extension_sector_6.csv'
 RUTA_INTERIOR_SECTOR_5 = f'{RUTA_FOLDER_DATA}zona_interior_sector_5.csv'
 RUTA_INTERIOR_SECTOR_6 = f'{RUTA_FOLDER_DATA}zona_interior_sector_6.csv'
-RUTA_INFO_PUNTOS = f'{RUTA_FOLDER_DATA}info_puntos.csv'
+RUTA_INFO_PUNTOS = f'{RUTA_FOLDER_DATA}input_sectores_5_6.csv'
 RUTA_FOLDER_OUTPUT = f'output/ciclo_{CICLO}/'
 
 RUTA_FOLDER_FIGURAS = f'{RUTA_FOLDER_OUTPUT}figuras/'
-RUTA_FOLDER_MALLAS = f'{RUTA_FOLDER_OUTPUT}malllas/'
+RUTA_FOLDER_MALLAS = f'{RUTA_FOLDER_OUTPUT}mallas/'
+RUTA_FOLDER_SWW = f'{RUTA_FOLDER_OUTPUT}swws/'
 
 def ejecutar_dia(ruta_topografia: str, sector: int,  dia: int, info_puntos: pd.DataFrame,
-                 yieldstep: float=400, tiempo_extra: float=1600):
+                 yieldstep: float=400, tiempo_extra: float=1600) -> str:
     '''
     Ejecuta el modelo ANUGA para un día
 
@@ -33,6 +37,9 @@ def ejecutar_dia(ruta_topografia: str, sector: int,  dia: int, info_puntos: pd.D
         ruta_topografia (str): Ruta del archivo de topografía
         sector (int): Sector a simular
         info_puntos (pd.DataFrame): Información de los puntos de control
+    
+    Returns:
+        str: Ruta de la topografía actualizada
     '''
 
     if sector == 5:
@@ -44,11 +51,16 @@ def ejecutar_dia(ruta_topografia: str, sector: int,  dia: int, info_puntos: pd.D
         ruta_interior = RUTA_INTERIOR_SECTOR_6
         ruta_extension = RUTA_EXTENSION_SECTOR_6
 
+    # Creamos carpetas relevantes en caso de no existir
+    carpeta_figuras = f'{RUTA_FOLDER_FIGURAS}{dia}/{sector}'
+    if not os.path.exists(carpeta_figuras):
+        os.makedirs(carpeta_figuras)
+
     simulador = AnugaSW(ruta_topografia, p.RUTA_MASK_TRANQUE,
                     ruta_region, ruta_interior, ruta_extension,
-                    nombre_salida_sww=f'relaves_{dia}_{sector}',
-                    carpeta_figuras=f'{RUTA_FOLDER_FIGURAS}{dia}/{sector}',
-                    mesh_filename=f'{RUTA_FOLDER_MALLAS}_{dia}_{sector}.msh',
+                    nombre_salida_sww=f'{RUTA_FOLDER_SWW}relaves_{dia}_{sector}',
+                    carpeta_figuras=carpeta_figuras,
+                    mesh_filename=f'{RUTA_FOLDER_MALLAS}malla_{dia}_{sector}.msh',
                     res_region=RES_REGION, res_interior=RES_INTERIOR)
     simulador.ejecutar(info_puntos, yieldstep=yieldstep, tiempo_extra=tiempo_extra)
 
@@ -62,3 +74,45 @@ def ejecutar_dia(ruta_topografia: str, sector: int,  dia: int, info_puntos: pd.D
 
     with open(f'{output_depth_folder}depth_{dia}_{sector}.pkl', 'wb') as f:
         pickle.dump(simulador.depth.dem, f)
+
+    # Actualizamos topografia
+    min_x, min_y = simulador.depth.dem.columns.min(), simulador.depth.dem.index.min()
+    max_x, max_y = simulador.depth.dem.columns.max(), simulador.depth.dem.index.max()
+    topo_act = deepcopy(simulador.topografia.dem)
+    topo_act.loc[max_y:min_y, min_x:max_x] = simulador.depth.dem
+
+    # Guardamos la topografia
+    ruta_nueva_topo = f'{RUTA_FOLDER_OUTPUT}topografia_{dia + 1}_{sector}.asc'
+    df_to_asc(topo_act, ruta_nueva_topo)
+
+    # Borramos la topografia antigua
+
+    if dia != 0:
+        os.remove(ruta_topografia)
+
+    return ruta_nueva_topo
+
+if __name__ == "__main__":
+    info_puntos = pd.read_csv(RUTA_INFO_PUNTOS, parse_dates=['fecha'])
+    fechas = sorted(info_puntos.fecha.unique())
+    fechas = [fecha.strftime("%Y-%m-%d") for fecha in fechas]
+
+    info_puntos = info_puntos.set_index(['fecha', 'sector'], drop=False)
+
+    ruta_topografia = RUTA_TOPOGRAFIA_BASE
+    
+    dias = list(range(len(fechas)))
+
+    print(f'Comenzando simulación del CICLO {CICLO}\n')
+    total_dias = len(dias) - 1
+    for dia, fecha in zip(dias, fechas):
+        print(f'Comenzando simulación DÍA {dia}/{total_dias}')
+
+        sectores = sorted(info_puntos.loc[info_puntos.fecha == fecha, 'sector'].unique())
+        for sector in sectores:
+            print('     Simulando SECTOR', sector)
+            info_puntos_local = info_puntos.loc[(fecha, sector),]
+            ruta_topografia = ejecutar_dia(ruta_topografia, sector, dia, info_puntos_local)
+        print()
+    print('Simulación terminada!')
+
